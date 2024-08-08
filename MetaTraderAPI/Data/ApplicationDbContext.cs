8b12041f89456e874e54c41b2bec18f796e4d2a0
juancaval26 <juancamilovalderrama@gmail.com>
2024-08-07 23:32:11 -0500

    Ahora a hacer las cosas bien

--- MetaTraderAPI/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using MetaTraderAPI.Models;
-
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-    public DbSet<AccountInfo> AccountInfos { get; set; }
-}
--- MetaTraderAPI/startup.cs
+++
@@ -1,7 +0,0 @@
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddControllers();
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-    services.AddScoped<MetaTraderService>();
-}
