126926cdf0cb6fd93d3b6eb9caa141181d8a493d
juancaval26 <juancamilovalderrama@gmail.com>
2024-08-20 23:07:36 -0500

    In process of the best project

--- metatraderproject/cuentas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('cuentas/', include('cuentas.urls')),
-    path('inversiones/', include('inversiones.urls')),
-    # otras rutas
-]
--- metatraderproject/metatraderproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cuentas/', include('cuentas.urls')),
-    path('inversiones/', include('inversiones.urls')),
-    # otras rutas
-]
